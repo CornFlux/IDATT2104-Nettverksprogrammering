64b182339ef0ab34b8169a4974d52b845ab34df1
CornFlux <50301728+CornFlux@users.noreply.github.com>
2021-02-23 16:09:29 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        Tjener tjener = new Tjener();
+        tjener.start();
+    }
+}
